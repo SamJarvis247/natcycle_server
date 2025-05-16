4adc3a8bdb503886000a6d1d47e82ee8793992f3
Prince <ograindy@gmail.com>
2025-05-16 20:44:26 +0100

    update

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -5,7 +5,7 @@ module.exports = {
     "https://www.natcycle.app",
     "http://localhost:5173",
     "https://nat-cycle.vercel.app",
-    "https://natcycle.netlify.app/",
+    "https://natcycle.netlify.app",
   ],
   optionSuccessStatus: 200,
   credentials: true,
