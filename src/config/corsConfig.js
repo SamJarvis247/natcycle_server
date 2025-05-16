8525ac17381f4da0be66f14942b00f985135c74c
Prince <ograindy@gmail.com>
2025-05-16 20:39:09 +0100

    fix(corsConfig): add "https://natcycle.netlify.app/" to allowed origins
    This commit adds "https://natcycle.netlify.app/" to the list of allowed origins in the CORS configuration to ensure proper access for the Netlify deployment.

--- src/config/corsConfig.js
+++ src/config/corsConfig.js
@@ -1,7 +1,12 @@
 // cors config file
 
 module.exports = {
-  origin: ['https://www.natcycle.app', 'http://localhost:5173', 'https://nat-cycle.vercel.app'],
+  origin: [
+    "https://www.natcycle.app",
+    "http://localhost:5173",
+    "https://nat-cycle.vercel.app",
+    "https://natcycle.netlify.app/",
+  ],
   optionSuccessStatus: 200,
-  credentials: true
-}
+  credentials: true,
+};
