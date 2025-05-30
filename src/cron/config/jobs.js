d1056519a3f6fce958f7b507ee24a43a6ed63e2d
Prince <ograindy@gmail.com>
2025-05-30 16:15:14 +0100

    fix(jobs): update keepAlive schedule to run every 14 minutes

--- src/cron/config/jobs.js
+++ src/cron/config/jobs.js
@@ -1,7 +1,7 @@
 module.exports = {
   keepAlive: {
     name: "Keep Server Alive",
-    schedule: "*/1 * * * *", // Every 1 minute
+    schedule: "*/14 * * * *", // Every 14 minutes
     timezone: "UTC",
     enabled: true,
     jobPath: "./jobs/keepAlive",
